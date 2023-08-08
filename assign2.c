2c6077c1a1b5d7755357720f764e392ada04b917
sakshiavinashe <140818917+Sakshiavinashe@users.noreply.github.com>
2023-08-08 15:37:46 +0530

    Add files via upload

---
+++ assign2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+ int num,num1;
+ scanf("%d\n%d",&num,&num1);
+ 	if(num>num1)
+ 	{
+  	printf("%d",num);
+  	}
+ 	else
+ 	{
+ 	printf("%d",num1);
+ 	}
+ return 0;
+ }
+
---
+++ assign3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c;
+scanf("%d\n%d\n%d",&a,&b,&c);
+if(a>b)
+{
+printf("a is greater");
+}
+else if(b>c)
+{
+printf("b is greater");
+}
+else 
+{
+printf("c is greater");
+}
+return 0;
+}
